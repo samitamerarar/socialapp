c2110e84579dfd69cc76a61b207d7370ff9bdaaf
samit574 <tamer.arar@polymtl.ca>
2020-01-17 17:49:20 -0500

    Upload image route

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -1,11 +1,12 @@
 const functions = require('firebase-functions');
-
 const app = require('express')();
-
 const FBAuth = require('./util/fbAuth');
 
+const cors = require('cors');
+app.use(cors());
+
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
+const { signup, login, uploadImage } = require('./handlers/users');
 
 // screams routes
 app.get('/screams', getAllScreams);
@@ -13,6 +14,7 @@ app.post('/scream', FBAuth, postOneScream);
 
 // users routes
 app.post('/signup', signup);
-app.post('/login', login)
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
--- socialapp-functions/functions/util/fbAuth.js
+++ socialapp-functions/functions/util/fbAuth.js
@@ -1,4 +1,4 @@
-const { admin } = require('./admin');
+const { admin, db } = require('./admin');
 
 module.exports = (req, res, next) => {
     let idToken;
@@ -13,7 +13,6 @@ module.exports = (req, res, next) => {
     admin.auth().verifyIdToken(idToken)
         .then(decodedToken => {
             req.user = decodedToken;
-            console.log(decodedToken);
             return db.collection('users')
                 .where('userId', '==',  req.user.uid)
                 .limit(1)
@@ -21,6 +20,7 @@ module.exports = (req, res, next) => {
         })
         .then(data => {
             req.user.handle = data.docs[0].data().handle;
+            req.user.imageUrl = data.docs[0].data().imageUrl;
             return next();
         })
         .catch(err => {
