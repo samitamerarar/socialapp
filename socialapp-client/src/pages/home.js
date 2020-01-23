2bed5d9a2d76e887f395f17e81d3e1073b46f673
samit574 <tamer.arar@polymtl.ca>
2020-01-23 15:40:32 -0500

    added react client

---
+++ socialapp-client/src/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import '../App.css';
+
+class home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>home page</h1>
+            </div>
+        )
+    }
+}
+
+export default home
